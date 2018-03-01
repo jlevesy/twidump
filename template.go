2d683f01cdf83de2543ee2892e7a3f79539cfef1
Julien Levesy <jlevesy@tea-ebook.com>
2018-03-01 14:04:39 +0100

    dealing with tweets

--- template.go
+++ template.go
@@ -1,10 +1,18 @@
 package main
 
 const templateContent = `
-{{ range $tweet := .Tweets}}
+{{ range $tweet := .}}
 From : {{$tweet.User.ScreenName}}  - Favorites : {{$tweet.FavoriteCount}} - Retweets : {{$tweet.RetweetCount}}
 ---------------------------------------------------
-{{$tweet.Text}}
+{{ if $tweet.Truncated }} Truncated {{ end }}
+{{ if $tweet.Retweeted }} Retweeted {{ end }}
+{{ if $tweet.ExtendedTweet }} Has Extended Tweet {{ end }}
+{{ if $tweet.RetweetedStatus }} Has Retweeted Status {{ end }}
+
+{{ if $tweet.Text }} {{ $tweet.Text }} {{ end }}
+{{ if $tweet.FullText }} {{ $tweet.FullText }} {{ end }}
 ---------------------------------------------------
+{{ else }}
+No tweets for that user :(
 {{ end }}
 `
